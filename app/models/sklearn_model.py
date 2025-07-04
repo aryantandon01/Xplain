467f90291b289579f366ab9b78045b8b0b7a582a
aryantandon01 <aryantandon01@gmail.com>
2025-07-04 16:21:23 +0530

    Moved sklearn training into single file

--- app/models/sklearn_model.py
+++ app/models/sklearn_model.py
@@ -1,6 +1,11 @@
 # app/models/sklearn_model.py
-import joblib
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
 
+# Load dataset and train at import time (only once)
+X, y = load_iris(return_X_y=True)
+sk_model = RandomForestClassifier()
+sk_model.fit(X, y)
 
 def load_model():
-    return joblib.load("models/model.pkl")
+    return sk_model
